be8dbddc2c0c9226deb042375184824aa89ed909
JeremyHooper <jerry.hooper@bigpond.com>
2023-12-29 03:39:28 +0800

    Update Folder

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "388d164c085ad1e938d1955d67396950"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d4649dcd7f7a40bf80a9245da628f6bb"
